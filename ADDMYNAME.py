dd6cb73f2744f1ebf944e17d7d7e5adcfcfe09cb
Cade Ferguson <cade_ferguson1@baylor.edu>
2021-12-01 17:06:57 -0600

    iop

---
+++ ADDMYNAME.py
@@ -0,0 +1,10 @@
+    
+def main():
+    
+    outfile = open("philosophers.txt",'a')
+    
+    outfile.write('\nCade Ferguson')
+
+    outfile.close()
+    
+main()
---
+++ DictionaryAssignemtn.py
@@ -0,0 +1,3 @@
+Course_Info = {'CS101': 3004,'CS102':4501,'CS103':6755,'NT110':1244,'CM241':1411}
+
+print(Course_Info)
